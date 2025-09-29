730e7ae7069fd220407d499c185df85b7e3dd9ce
MANUEL SERRANO RUBIO <Manuel.Serrano10@alu.uclm.es>
2025-09-29 20:28:18 +0200

    Creadas las clases unicas a mi caso de uso

---
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -0,0 +1,61 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Rental
+    {
+        public Rental() { 
+        
+        }
+
+        public Rental(int id, string name, string surname, DateTime endDate, DateTime startDate, DateTime rentingDate,
+            int totalPrice, string deliveryCarDealer, IList<RentalItem> rentalitem)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            EndDate = endDate;
+            StartDate = startDate;
+            RentingDate = rentingDate;
+            TotalPrice = totalPrice;
+            DeliveryCarDealer = deliveryCarDealer;
+            RentalItems = rentalitem;
+        }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime RentingDate { get; set; }
+        public int TotalPrice { get; set; }
+        public string DeliveryCarDealer { get; set; }
+        public IList<RentalItem> RentalItems { get; set; }
+        public override bool Equals(object? obj)
+        {
+            return obj is Rental rental &&
+                   Id == rental.Id &&
+                   Name == rental.Name &&
+                   Surname == rental.Surname &&
+                   EndDate == rental.EndDate &&
+                   StartDate == rental.StartDate &&
+                   RentingDate == rental.RentingDate &&
+                   TotalPrice == rental.TotalPrice &&
+                   DeliveryCarDealer == rental.DeliveryCarDealer &&
+                   RentalItems == rental.RentalItems;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(Id);
+            hash.Add(Name);
+            hash.Add(Surname);
+            hash.Add(EndDate);
+            hash.Add(StartDate);
+            hash.Add(RentingDate);
+            hash.Add(TotalPrice);
+            hash.Add(DeliveryCarDealer);
+            hash.Add(RentalItems);
+            return hash.ToHashCode();
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -0,0 +1,33 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class RentalItem
+    {
+        public RentalItem() { }
+
+        public RentalItem(int carId, int quantity, int rentalId)
+        {
+            CarId = carId;
+            Quantity = quantity;
+            RentalId = rentalId;
+        }
+        [Key]
+        public int CarId { get; set; }
+        public int Quantity { get; set; }
+        [Key]
+        public int RentalId { get; set; }
+        public Rental Rental { get; set; }
+        public override bool Equals(object? obj)
+        {
+            return obj is RentalItem item &&
+                   CarId == item.CarId &&
+                   Quantity == item.Quantity &&
+                   RentalId == item.RentalId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(CarId, Quantity, RentalId);
+        }
+    }
+}
