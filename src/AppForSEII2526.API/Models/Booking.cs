d32921b1eb9324c6bf1f68ea7f197ff01bd8b5d3
Daniel <fynalcloud@gmail.com>
2025-09-29 20:24:09 +0200

    Añadidas las clases Booking, bookingitem, maintentance, maintenancetype, sin las relaciones entre ellas aun

---
+++ src/AppForSEII2526.API/Models/Booking.cs
@@ -0,0 +1,43 @@
+using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Booking
+    {
+        public Booking()
+        {
+
+        }
+        public Booking(string clientAddress, string clientName, int clientPhoneNumber, string clientSurname, string Date, int Id, string PaymentMethod) : base()
+        {
+            this.clientAdress = clientAddress;
+            this.clientName = clientName;
+            this.clientPhoneNumber = clientPhoneNumber;
+            this.clientSurname = clientSurname;
+            this.Date = Date;
+            this.Id = Id;
+            this.PaymentMethod = PaymentMethod;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string clientAdress { get; set; }
+        public string clientName { get; set; }
+        public int clientPhoneNumber { get; set; }
+        public string clientSurname { get; set; }
+        public string Date { get; set; }
+        public string PaymentMethod { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Booking booking &&
+                this.Id == booking.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, clientName);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/Models/Maintenance.cs
@@ -0,0 +1,37 @@
+﻿using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Maintenance
+    {
+        public Maintenance()
+        {
+
+        }
+        public Maintenance(int Id, string Name, int NumberOfDays, float Price) : base()
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.NumberOfDays = NumberOfDays;
+            this.Price = Price;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfDays { get; set; }
+        public float Price { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Maintenance maintenance &&
+                this.Id == maintenance.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+    }
+}
