376721b6b93be7f09a6ef343583e28d197b2bbb0
JUAN JOSÉ ARZOLA PIÑAS <JuanJose.Arzola@alu.uclm.es>
2025-10-07 12:21:05 +0200

    He creado la clase PurchaseItem

---
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -0,0 +1,54 @@
+﻿
+namespace AppForSEII2526.API.Models
+{
+    public class PurchaseItem
+    {
+
+        public int CarId, PurchaseId, Quantity;
+
+        //public Purchase purchase;
+
+        public Car car;
+
+        public PurchaseItem()
+        {
+
+        }
+
+        public PurchaseItem(int carId, int purchaseId, int quantity)
+        {
+
+            CarId = carId;
+
+            PurchaseId = purchaseId;
+
+            Quantity = quantity;
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+
+            return obj is PurchaseItem item &&
+
+                CarId == item.CarId &&
+
+                PurchaseId == item.PurchaseId &&
+
+                Quantity == item.Quantity;
+
+        }
+
+        public override int GetHashCode()
+        {
+
+            return HashCode.Combine(CarId, PurchaseId, Quantity);
+
+        }
+
+
+
+    }
+
+
+}
