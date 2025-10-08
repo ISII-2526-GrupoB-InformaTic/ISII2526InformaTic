1408c5599af1ebe9ca9ecf22d9723eb2aae416ec
MANUEL SERRANO RUBIO <Manuel.Serrano10@alu.uclm.es>
2025-10-08 19:58:43 +0200

    Añadidas las relaciones necesarias y eliminados los atributos correspondientes a ApplicationUser que estaban en Purchase y Rental

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,14 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    
+
+    public IList<Purchase> Purchases { get; set; }
+    public IList<Rental> Rentals { get; set; }
+
+    public ApplicationUser(IList<Purchase> purchases, IList<Rental> rentals)
+    {
+        Purchases = purchases;
+        Rentals = rentals;
+    }
 }
