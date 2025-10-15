bbaf872958b2ff54cb7dc9665b40ae9eb8af53a1
MANUEL SERRANO RUBIO <Manuel.Serrano10@alu.uclm.es>
2025-10-15 10:09:08 +0200

    Añadido el PaymentMethod a la clase Rental

--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -9,7 +9,7 @@ namespace AppForSEII2526.API.Models
         }
 
         public Rental(int id, DateTime endDate, DateTime startDate, DateTime rentingDate,
-            int totalPrice, string deliveryCarDealer, IList<RentalItem> rentalItems)
+            int totalPrice, string deliveryCarDealer, IList<RentalItem> rentalItems, PaymentMethod paymentMethod)
         {
             Id = id;
             EndDate = endDate;
@@ -18,32 +18,45 @@ namespace AppForSEII2526.API.Models
             TotalPrice = totalPrice;
             DeliveryCarDealer = deliveryCarDealer;
             RentalItems = rentalItems;
+            PaymentMethod = paymentMethod;
+
         }
         [Key]
         public int Id { get; set; }
+
         [Required]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
+
         [Required]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
+
         [Required]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime RentingDate { get; set; }
+
         [Required]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
         [Range(1, 1000000, ErrorMessage = "Minimum 1, Maximum 1000000")]
         [Precision(5,2)]
         public int TotalPrice { get; set; }
+
         [Required]
         [StringLength(50, ErrorMessage = "DeliveryCarDealer can be neither longer than 50 characters nor shorter than 10.", MinimumLength=10)]
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         public string DeliveryCarDealer { get; set; }
+
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; }
+
         public IList<RentalItem> RentalItems { get; set; }
+
         public ApplicationUser User { get; set; }
+
         public override bool Equals(object? obj)
         {
             return obj is Rental rental &&
@@ -53,7 +66,8 @@ namespace AppForSEII2526.API.Models
                    RentingDate == rental.RentingDate &&
                    TotalPrice == rental.TotalPrice &&
                    DeliveryCarDealer == rental.DeliveryCarDealer &&
-                   RentalItems == rental.RentalItems;
+                   RentalItems == rental.RentalItems &&
+                   PaymentMethod == rental.PaymentMethod;
 
         }
 
@@ -67,6 +81,7 @@ namespace AppForSEII2526.API.Models
             hash.Add(TotalPrice);
             hash.Add(DeliveryCarDealer);
             hash.Add(RentalItems);
+            hash.Add(PaymentMethod);
             return hash.ToHashCode();
         }
     }
