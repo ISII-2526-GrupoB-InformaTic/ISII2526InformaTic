c389aa5fdf87f89d8be1da8cd9352bd54d66dc8f
MANUEL SERRANO RUBIO <Manuel.Serrano10@alu.uclm.es>
2025-10-22 09:45:23 +0200

    Creada la clase CarController y los DTOs necesarios para los alquileres

---
+++ src/AppForSEII2526.API/Controllers/CarsController.cs
@@ -0,0 +1,53 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        //used to enable your controller to access to the database
+        private readonly ApplicationDbContext _context;
+        //used to log any information when your system is running
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ApplicationDbContext context, ILogger<CarsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if (op2 == 0)
+            {
+                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+                return BadRequest("op2 must be different from 0");
+            }
+            decimal result = decimal.Round(op1 / op2, 2);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CarForRentalDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetCarsForRenting(string? modelFilter, int? priceMin, int? priceMax)
+        {
+            IList<CarForRentalDTO> cars = await _context.Cars
+                .Include(c => c.Model)
+                .Where(c => ((modelFilter == null) || (c.Model.Name.Equals(modelFilter))) &&
+                    ((c.RentingPrice <= priceMax) || (priceMax==null)) &&
+                    ((c.RentingPrice >= priceMin) || (priceMin==null)))
+                .OrderBy(c=> c.Model.Name)  
+                .Select(c => new CarForRentalDTO(c.Id,c.Color,c.Manufacturer,c.RentingPrice,c.FuelType,c.Model))
+                .ToListAsync();
+            return Ok(cars);
+        }
+    }
+}
