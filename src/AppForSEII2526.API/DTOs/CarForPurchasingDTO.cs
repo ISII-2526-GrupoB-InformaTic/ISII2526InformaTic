d746b87e1a0cb1ca1f5e0feb58d03a1914314006
JuanJoseArzola <juanjose.arzola@alu.uclm.es>
2025-10-25 16:30:30 +0200

    70 creacion de controlador car (#92) (#93)
    * Añadiendo a la rama la base de datos actualizada (#82)
    
    * Solucionados problemas con la clase Car (#74)
    
    * Actualizada la base de datos y la migracion (#79)
    
    ---------
    
    
    
    * He añadidos un Get para las compras de los coches
    
    ---------
    
    Co-authored-by: ManuelSerranoRubio <Manuel.Serrano10@alu.uclm.es>

---
+++ src/AppForSEII2526.API/DTOs/CarForPurchasingDTO.cs
@@ -0,0 +1,39 @@
+﻿using System.Drawing;
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class CarForPurchasingDTO
+    {
+
+        public CarForPurchasingDTO(int id, Model Model,String Color, String FuelType, String Manufacture, int PurchasingPrice)
+        {
+
+            Id = id;
+
+            model = Model;
+
+            color = Color;
+
+            fuelType = FuelType;
+
+            manufacture = Manufacture;
+
+            purchasingPrice = PurchasingPrice;
+
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(50, ErrorMessage = "Title name cannot be longer than 50 characters. ")]
+        public Model model { get; set; }
+
+        public String color { get; set; }
+
+        public String fuelType { get; set; }
+
+        public String manufacture { get; set; }
+
+        public int purchasingPrice { get; set; }
+
+    }
+}
