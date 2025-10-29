9ca5d26b4351820adda3bf82b5f8c2404a832713
DanielPozM <Daniel.Pozo3@alu.uclm.es>
2025-10-29 08:41:54 +0100

    Creados los cambios en Cars controller, y añadidos los DTO (#94)
    Co-authored-by: Daniel <DanielPozo3@alu.uclm.es>

---
+++ src/AppForSEII2526.API/DTOs/MaintenanceTypeDTO.cs
@@ -0,0 +1,25 @@
+using AppForSEII2526.API.DTOs;
+
+namespace AppForSEII2526.API.DTOs
+{
+    public class MaintenanceTypeDTO
+    {
+        public MaintenanceTypeDTO()
+        {
+
+        }
+        public MaintenanceTypeDTO(int Id, string Type, Maintenance Maintenance) : base()
+        {
+            this.Id = Id;
+            this.Type = Type;
+            this.Maintenance = Maintenance;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(10, ErrorMessage = "El nombre debe tener menos de 10 caracteres y mas de 3.", MinimumLength = 3)]
+        public string Type { get; set; }
+        public Maintenance Maintenance { get; set; }
+
+    }
+}
